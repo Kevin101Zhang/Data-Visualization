9f49c2c91bcc05acc4033e7b11d05e06ef45bcb0
remidev <28935481+remidev@users.noreply.github.com>
2019-03-29 22:06:29 -0400

    initial commit

--- create-react-express/routes/index.js
+++ create-react-express/routes/index.js
@@ -3,8 +3,7 @@
 const { BigQuery } = require("@google-cloud/bigquery");
 
 // Set Credentials
-//const path =
-//"C:/Users/teddy/Downloads/decoded-reducer-234800-f824d46ed564.json";
+//const path = "C:/Users/teddy/Downloads/decoded-reducer-234800-f824d46ed564.json";
 const path = "/Users/kevinz/Downloads/decoded-reducer-234800-b532ca7bb227.json";
 process.env.GOOGLE_APPLICATION_CREDENTIALS = path;
 
@@ -14,7 +13,9 @@ async function query(source, contentType, content1, content2, year) {
   const bigquery = new BigQuery();
   // SQL Query String
   var sqlQuery = `SELECT ${content1},${content2} FROM \`${source}${contentType}.${year}\`LIMIT 10`;
-  console.log(`SELECT ${content1},${content2} FROM \`${source}${contentType}.${year}\`LIMIT 10`);
+  console.log(
+    `SELECT ${content1},${content2} FROM \`${source}${contentType}.${year}\`LIMIT 10`
+  );
   // Query Options,
   const options = {
     query: sqlQuery,
@@ -27,22 +28,20 @@ async function query(source, contentType, content1, content2, year) {
 router.post("/api/give", function(req, res) {
   console.log(req.body.queryInfo);
   params = req.body.queryInfo;
-  
+
   query(
     params.Source,
     "comments",
     params.Content1,
     params.Content2,
     "2005"
-  ).then(function(resOfQuery){
-      res.json(resOfQuery);
-      console.log("HELLO WORLD!");
+  ).then(function(resOfQuery) {
+    res.json(resOfQuery);
+    console.log("HELLO WORLD!");
   });
 });
 
 module.exports = router;
-
-
 
 // router.get("/api/all", function(req, res) {
 //   query().then(function(result) {
